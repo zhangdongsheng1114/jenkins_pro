5070235cda0a02c5cca4e355005738caeab07625
zhangdongsheng1114 <594611791@qq.com>
2020-03-29 11:51:10 +0800

    初始化提交

---
+++ scripts/test_contact.py
@@ -0,0 +1,18 @@
+
+
+class TestContact:
+
+    def test_contact1(self):
+        assert 1
+
+    def test_contact2(self):
+        assert 0
+
+    def test_contact3(self):
+        assert 1
+
+    def test_contact4(self):
+        assert 1
+
+    def test_contact5(self):
+        assert 1
